3ca71b8b4cae00d0b996a5b5021934de00b4e1e7
OmkarKhilari <omkarkhilari95@gmail.com>
2024-01-09 12:44:54 +0530

    main pushed

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:private_chat/components/custom_textfield.dart';
 import 'package:private_chat/components/sent_message.dart';
+import 'package:private_chat/screens/chat_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Our Chat App',
-      home: Scaffold(body: SafeArea(child: SentMessage(messageText: 'messageText', senderName: 'senderName'))),
+      home: Scaffold(body: SafeArea(child: Center(child: ChatPage()))),
     );
   }
 }
