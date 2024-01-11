917a712bdc41f2989d5e21d57de0d9098c8fd774
OmkarKhilari <omkarkhilari95@gmail.com>
2024-01-11 22:14:09 +0530

    app bar added

--- frontend/lib/components/appbar.dart
+++ frontend/lib/components/appbar.dart
@@ -1,14 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
 
+  //TODO: fetch room name and people count, hardcoded right now
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 64,
+      height: 62,
       width: double.infinity,
-      color: Color.fromARGB(255, 55, 55, 55),
+      color: Color(0xff000000).withOpacity(0.6),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Icon(
+              Icons.arrow_back,
+              size: 28,
+              color: Color(0xffFFFFFF),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 8,
+                ),
+                RichText(
+                    text: TextSpan(
+                        text: 'Da\'s',
+                        style: GoogleFonts.montserrat(
+                            fontSize: 21,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: -0.2,
+                            color: Color(0xffFFFFFF)))),
+                RichText(
+                  text: TextSpan(
+                    text: '11 people are in the room',
+                  style: GoogleFonts.montserrat(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xffBABABA)),
+                ))
+              ],
+            ),
+            const Spacer(),
+            Icon(
+              Icons.more_vert_rounded,
+              size: 28,
+              color: Color(0xffFFFFFF),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
